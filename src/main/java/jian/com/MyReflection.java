3a785c6a34e9029aac0dd67321613ea530fb55a0
xingjianhong <jianhong.xing@hunteron.com>
2016-05-06 10:24:10 +0800

    Revert "Revert "commit 22""
    This reverts commit e09174f63382c0df59f2586eafc0353751c2094a.

--- src/main/java/jian/com/MyReflection.java
+++ src/main/java/jian/com/MyReflection.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import jian.com.pojo.Foo;
 import jian.com.service.Fruit;
 
-public class MyReflection<T> {
+public class MyReflection {
 
     public static <T> Fruit reflectFactory(T t) {
         Fruit object = null;
